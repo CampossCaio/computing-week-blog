6e5e968524daac7735b4b19a47ebafd9d77cb23b
caiocampos-hotmart <82822777+caiocampos-hotmart@users.noreply.github.com>
2021-05-27 20:29:01 -0300

    feature:  add image plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-       <p>I'm making this by following the Gatsby Tutorial.</p>
+       <p>JAMstack</p>
+       <StaticImage
+        alt="JamStack"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7BSUOKH5RvRTGJIwoAihcTJ5hCG4okug9eg&usqp=CAU"
+      />
     </Layout>
   )
 }
