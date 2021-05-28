9d8f3bf31eaa3774e5dc972e4853add99025e34d
caiocampos-hotmart <82822777+caiocampos-hotmart@users.noreply.github.com>
2021-05-27 21:13:05 -0300

    add color

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import { StaticImage } from 'gatsby-plugin-image';
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-       <p>JAMstack</p>
+       <p style={{color:"pink"}}>Welcome to the JAMstack</p>
        <StaticImage
         alt="JamStack"
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7BSUOKH5RvRTGJIwoAihcTJ5hCG4okug9eg&usqp=CAU"
